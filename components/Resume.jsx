64bff7754303d97bede2fbf0d9d7e155613b7e19
richierichh <78582740+richierichh@users.noreply.github.com>
2024-07-30 22:36:44 -0400

    fixed resume download bug

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Resume = () => {
     const resumeLink = 'https://drive.google.com/file/d/1WvvowcHMm5gDYav4yixWJbVfCyc6CmXc/preview'; 
-    const resumeDownloadLink = 'https://drive.google.com/uc?export=download&id=1WvvowcHgMm5gDYav4yixWJbVfCyc6CmXc'; 
+    const resumeDownloadLink = 'https://drive.google.com/uc?export=download&id=1WvvowcHMm5gDYav4yixWJbVfCyc6CmXc'; 
     return (
 <div id="Resume"className="pt-28 p-20 flex flex-col justify-center items-center overflow-auto">
   <h1 className='text-center font-bold text-4xl mb-8 '>
